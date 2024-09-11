ee894a9a75c8489c628b748b9f1e34b7f5f0a2b7
Evans-mutuku <evansnyamai98@gmail.com>
2024-09-11 08:26:26 +0300

    second commit

--- mysite/weatherapp/views.py
+++ mysite/weatherapp/views.py
@@ -4,33 +4,12 @@ from django.shortcuts import render
 # Create your views here.
 
 
-# def index(request):
-#     if request.method == 'POST':
-#         city = request.POST['city']
-#         source =  urllib.request.urlopen('http://api-openweathermap.org/data/2.5/weather?q=' + city + '&units=metric&appid=d00b54acc2909f876bea1717d8e1fbab').read()
-#         data_list = json.loads(source)
-#         #  create data dictionary
-#         data = {"country_code": str(data_list['sys']['country']),
-#                  "coordinates" : str(data_list['coord']['lon']) + str(data_list['coord']['lat']),
-#                  "temp" : str(data_list['main']['temp'] + ' C'),
-#                  "pressure" : str(data_list['main']['pressure']),
-#                  "humidity" : str(data_list['main']['humidity']),
-#                  "main" : str(data_list['weather']['main']),
-#                  "description" : str(data_list['weather'][0]['description']),
-#                  "icon": str(data_list['weather'][0]['icon'])
-#                 }
-#         print(data)
-#     else:
-#         data = {}
-#     return render(request, "main/index.html", data)
-
-
 def index(request):
 
     if request.method == 'POST':
         city = request.POST['city']
 
-        source = urllib.request.urlopen('http://api-openweathermap.org/data/2.5/weather?q=' + city + '&units=metric&appid=d00b54acc2909f876bea1717d8e1fbab').read()
+        source = urllib.request.urlopen('http://api-openweathermap.org/data/2.5/weather?q=' + city + '&units=metric&appid=<API-KEY-GOES-HERE>').read()
         list_of_data = json.loads(source)
 
         data = {
